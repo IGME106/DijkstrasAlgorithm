92962f1ae011c262ce244db340f51836ef2e3ea1
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2018-04-20 10:12:44 -0400

    Import files from tree search

--- DijkstrasAlgorithm/Program.cs
+++ DijkstrasAlgorithm/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// IGME-106 - Game Development and Algorithmic Problem Solving
+/// Practice exercise 19
+/// Class Description   : Main program
+/// Author              : Benjamin Kleynhans
+/// Modified By         : Benjamin Kleynhans
+/// Date                : April 19, 2018
+/// Filename            : Program.cs
+/// </summary>
 namespace DijkstrasAlgorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Graph myGraph = new Graph();                                                    // Create graph object
+
+            try
+            {
+                myGraph.DepthFirst("l");                                                    // Start at node "L" and print graph.
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("\nPress enter to continue...");
+            Console.ReadLine();
         }
     }
 }
